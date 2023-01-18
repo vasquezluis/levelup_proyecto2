b00d34cf036ec7dfa7275ab74b8ff79682f9dce2
LuiiVasquez <luivasque95@gmail.com>
2023-01-18 14:17:40 -0600

    creatting temporary object

--- src/controllers/items.ts
+++ src/controllers/items.ts
@@ -1,20 +1,44 @@
 import path from "path";
 import fs from "fs";
 import { Request, Response } from "express";
+import { response } from "../common/response";
 
-const archivo = path.resolve("../config", "data.json");
-fs.readFile(archivo, "utf-8", (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.parse(data));
-  }
-});
+// const archivo = path.resolve("../config", "data.json");
+// fs.readFile(archivo, "utf-8", (error, data) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(JSON.parse(data));
+//   }
+// });
+
+const items: { id: number; name: string; age: number }[] = [
+  {
+    id: 0,
+    name: "Luis Vasquez",
+    age: 23,
+  },
+  {
+    id: 1,
+    name: "Antonio Tiu",
+    age: 24,
+  },
+  {
+    id: 2,
+    name: "Jorge Hernandez",
+    age: 25,
+  },
+  {
+    id: 3,
+    name: "Hugo Garcia",
+    age: 26,
+  },
+];
 
 export const getItems = (req: Request, res: Response) => {
   try {
-    res.json("hola");
+    res.json(items);
   } catch (error) {
-    res.status(500).json({ message: "error" });
+    response.error(res);
   }
 };
