c82811d00f6ddc64289eab2ae344c4c22334dc0a
LuiiVasquez <luivasque95@gmail.com>
2023-01-17 22:59:42 -0600

    adding router for all routes

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
+import { router } from "./routes";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // routes
+app.use(router);
 
 const PORT = process.env.PORT;
 
