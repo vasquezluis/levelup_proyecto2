cb91f7ce814d318a32b45a450e332a2dfee13d2f
LuiiVasquez <luivasque95@gmail.com>
2023-01-18 16:40:11 -0600

    adding routes

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,21 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
-import { router } from "./routes";
+import { swaggerDocs } from "./v1/swagger";
+import indexRoutes from "./v1/routes/index";
+import itemsRoutes from "./v1/routes/items";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // routes
-app.use(router);
+app.use(indexRoutes);
+app.use(itemsRoutes);
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
+  swaggerDocs(app, PORT);
 });
