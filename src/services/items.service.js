ab480d63041850144b6d06ae3c5ab219108c4470
LuiiVasquez <luivasque95@gmail.com>
2023-01-21 08:57:00 -0600

    creating services file for items

---
+++ src/services/items.service.js
@@ -0,0 +1,14 @@
+export const getItemsService = () => {
+  const items = [
+    {
+      id: 1,
+      name: "laptop",
+    },
+    {
+      id: 2,
+      name: "mouse",
+    },
+  ];
+
+  return items
+};
