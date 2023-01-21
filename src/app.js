d1c3eccc283de3b4afa145766c90dd69c3702735
LuiiVasquez <luivasque95@gmail.com>
2023-01-21 12:04:21 -0600

    adding all routes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,11 @@ import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express";
 
 import indexRoutes from "./v1/routes/index.js";
-import itemsRoutes from "./v1/routes/items.routes.js";
+// import ingresosRoutes from "./v1/routes/ingresos.routes.js";
+// import inventarioRoutes from "./v1/routes/inventario.routes.js";
+// import lineasRoutes from "./v1/routes/lineas.routes.js";
+import marcasRoutes from "./v1/routes/marcas.routes.js";
+// import ventasRoutes from "./v1/routes/ventas.routes.js";
 
 // Docs in json fromat
 import swaggerOptionsFile from "./swagger.json" assert { type: "json" };
@@ -19,6 +23,10 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 // routes
 app.use(indexRoutes);
-app.use(itemsRoutes);
+// app.use(ingresosRoutes);
+// app.use(inventarioRoutes);
+// app.use(lineasRoutes);
+app.use(marcasRoutes);
+// app.use(ventasRoutes);
 
 export default app;
