075b8c76594973fa1196b45a5bd1e5d9d6a372fe
LuiiVasquez <luivasque95@gmail.com>
2023-01-21 12:47:52 -0600

    changing swagger config

--- src/app.js
+++ src/app.js
@@ -11,17 +11,27 @@ import indexRoutes from "./v1/routes/index.js";
 import marcasRoutes from "./v1/routes/marcas.routes.js";
 // import ventasRoutes from "./v1/routes/ventas.routes.js";
 
-// Docs in json fromat
+const app = express();
+
+/**
+ * * swagger config
+ */
 import swaggerOptionsFile from "./swagger.json" assert { type: "json" };
 const specs = swaggerJSDoc(swaggerOptionsFile);
+// swaggerOptionsFile.definition.servers[0].url = headerChanged;
+// console.log(req.params)
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
-const app = express();
+/**
+ * * app config
+ */
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
-// routes
+/**
+ * * routes
+ */
 app.use(indexRoutes);
 // app.use(ingresosRoutes);
 // app.use(inventarioRoutes);
