d792bc2e8aef24ca4e54e7220b9913e8b99c71eb
LuiiVasquez <luivasque95@gmail.com>
2023-01-21 18:04:22 -0600

    adding lineas route

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -5,6 +5,7 @@ const router = Router();
 router.get("/", ({ headers: { host } }, res) => {
   const menu = {
     docs: `http://${host}/api-docs`,
+    lineas: `http://${host}/lineas`,
     marcas: `http://${host}/marcas`,
   };
 
