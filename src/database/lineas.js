ab7f8421cce4ff9a72d4144fbcad81b110c20d9f
LuiiVasquez <luivasque95@gmail.com>
2023-01-23 16:43:31 -0600

    creating lineas array for database

---
+++ src/database/lineas.js
@@ -0,0 +1,38 @@
+let lines = [
+  {
+    id: 1,
+    nombre: "Playera",
+    activo: true,
+    descripcion:
+      "Prenda de ropa interior o deportiva, por lo general tiene mangas cortas, cuello redondo o en forma de 'V'.",
+  },
+  {
+    id: 2,
+    nombre: "Sudadera",
+    activo: true,
+    descripcion:
+      "Prenda deportiva de algodón u otro tejido, cerrada de escote redondo y manga larga. Suele llevarse sobre una camiseta.",
+  },
+  {
+    id: 3,
+    nombre: "Pants",
+    activo: true,
+    descripcion:
+      "Tipo de pantalón diseñado para practicar actividad deportiva o brindar comodidad al descansar.",
+  },
+  {
+    id: 4,
+    nombre: "Tenis",
+    activo: false,
+    descripcion: "Calzado especial que se usa para practicar ciertos deportes, de material flexible con goma flexible y suele llevar cordones.",
+  },
+  {
+    id: 5,
+    nombre: "Pantaloneta",
+    activo: true,
+    descripcion:
+      "Pantalón corto que se usa para hacer deporte o para bañarse.",
+  },
+];
+
+export default lines;
