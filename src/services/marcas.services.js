df48e3f9a507ae66c919bf0c0d88702aad6a45f3
LuiiVasquez <luivasque95@gmail.com>
2023-01-23 16:46:37 -0600

    adding connection to database for services operations

--- src/services/marcas.services.js
+++ src/services/marcas.services.js
@@ -1,37 +1,75 @@
+import databaseMarcas from "../database/marcas.js";
+
 export const getBrandsService = () => {
-  const brands = "Listado de todas marcas";
+  const brands = databaseMarcas;
 
   return brands;
 };
 
 export const getActiveBrandsService = () => {
-  const activeBrands = "Listado de marcas activas";
+  const activeBrands = databaseMarcas.filter((item) => item.activo == true);
 
   return activeBrands;
 };
 
 export const getBrandService = (id) => {
-  const brand = `Marca ${id}`;
+  const brand = databaseMarcas.find((item) => item.id === parseInt(id));
 
   return brand;
 };
 
-export const createBrandService = (body) => {
-  const newBrand = body;
+export const createBrandService = ({ nombre, descripcion }) => {
+  const id = Math.max(...databaseMarcas.map((item) => item.id)) + 1;
+
+  const newBrand = {
+    id,
+    nombre,
+    activo: true,
+    descripcion,
+  };
+
+  databaseMarcas.push(newBrand);
 
   return newBrand;
 };
 
-export const updateBrandService = (id, { nombre, descripcion }) => {
-  console.log(nombre, descripcion);
+export const updateBrandService = (id, { nombre, activo, descripcion }) => {
+  const brandFound = databaseMarcas.find((item) => item.id === parseInt(id));
 
-  const updatedBrand = `Marca ${id} actualizada con los datos: ${nombre} ${descripcion}`;
+  const oldActive = brandFound.activo;
 
-  return updatedBrand;
+  let newActive = null;
+
+  if (brandFound) {
+    /**
+     * * Crear nuevo valor de activo
+     * ! activo pueder tener valor o no, segun los datos a actualizar
+     */
+    if (activo !== undefined) {
+      newActive = activo;
+    }
+    if (activo == undefined) {
+      newActive = oldActive;
+    }
+
+    nombre ? (brandFound.nombre = nombre) : nombre;
+    brandFound.activo = newActive;
+    descripcion ? (brandFound.descripcion = descripcion) : descripcion;
+
+    return brandFound;
+  } else {
+    return null;
+  }
 };
 
 export const deleteBrandService = (id) => {
-  const deletedBrand = `Marca ${id} eliminada`;
+  const brandFound = databaseMarcas.find((item) => item.id === parseInt(id));
 
-  return deletedBrand;
+  if (brandFound) {
+    brandFound.activo = false;
+
+    return brandFound;
+  } else {
+    return null;
+  }
 };
