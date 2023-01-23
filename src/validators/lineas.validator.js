bba9d4aaf794b3f8a3686a6c04c9d14d1d3bd40c
LuiiVasquez <luivasque95@gmail.com>
2023-01-23 16:48:12 -0600

    adding params & updateValidators

--- src/validators/lineas.validator.js
+++ src/validators/lineas.validator.js
@@ -1,5 +1,19 @@
-import { check } from "express-validator";
+import { check, param } from "express-validator";
 import { validateResult } from "../helpers/validateHelper.js";
+
+export const paramsValidation = [
+  /**
+   * * PARAMS: id (int)
+   */
+
+  param("id")
+    .exists()
+    .isNumeric()
+    .withMessage("El id debe ser un tipo de dato numerico"),
+  (req, res, next) => {
+    validateResult(req, res, next);
+  },
+];
 
 export const createValidation = [
   /**
@@ -19,6 +33,34 @@ export const createValidation = [
     .withMessage("El campo descripcion debe ser un string")
     .notEmpty()
     .withMessage("El campo descripcion no debe estar vacio"),
+  (req, res, next) => {
+    validateResult(req, res, next);
+  },
+];
+
+export const updateValidation = [
+  /**
+   * * LINEA: nombre, activo, descripcion
+   */
+
+  check("nombre")
+    .isString()
+    .withMessage("El campo nombre deber ser un string")
+    .notEmpty()
+    .withMessage("El campo nombre no debe estar vacio")
+    .optional(),
+  check("activo")
+    .isBoolean()
+    .withMessage("El campo activo debe ser un booleano")
+    .notEmpty()
+    .withMessage("El campo activo no debe estar vacio")
+    .optional(),
+  check("descripcion")
+    .isString()
+    .withMessage("El campo descripcion debe ser un string")
+    .notEmpty()
+    .withMessage("El campo descripcion no debe estar vacio")
+    .optional(),
   (req, res, next) => {
     validateResult(req, res, next);
   },
