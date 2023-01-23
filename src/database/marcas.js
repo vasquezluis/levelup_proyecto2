a728e68d6080d228352ae8e5bae537cefcc71a9c
LuiiVasquez <luivasque95@gmail.com>
2023-01-23 16:43:07 -0600

    creating marcas array for database

---
+++ src/database/marcas.js
@@ -0,0 +1,44 @@
+let brands = [
+  {
+    id: 1,
+    nombre: "Adidas",
+    activo: true,
+    descripcion:
+      "Compañia multinacional alemana dedicada a la fabricación de equipamiento deportivo.",
+  },
+  {
+    id: 2,
+    nombre: "Nike",
+    activo: true,
+    descripcion:
+      "Empresa multinacional estadounidense dedicada al diseño, desarrollo, fabricación y comercialización de equipamiento deportivo.",
+  },
+  {
+    id: 3,
+    nombre: "Puma",
+    activo: true,
+    descripcion:
+      "Empresa alemana fabrincate de accesorios, ropa y calzado deportivo.",
+  },
+  {
+    id: 4,
+    nombre: "Asics",
+    activo: false,
+    descripcion: "Empresa de material y ropa deportiva japonesa.",
+  },
+  {
+    id: 5,
+    nombre: "Reebok",
+    activo: true,
+    descripcion:
+      "Empresa estadounidense de zapatillas, ropta y accesorios deportivos.",
+  },
+  {
+    id: 6,
+    nombre: "Under Armour",
+    activo: false,
+    descripcion: "Empresa estadounidense de ropa y accesorios deportivos.",
+  },
+];
+
+export default brands;
