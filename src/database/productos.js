ce23a38cd6194d2494b2b1c0cdc269e32575badb
LuiiVasquez <luivasque95@gmail.com>
2023-01-24 14:31:19 -0600

    creating products database

---
+++ src/database/productos.js
@@ -0,0 +1,34 @@
+let products = [
+  {
+    id: 1,
+    nombre: "Playera Eternal",
+    activo: true,
+    descripcion:
+      "Playera negra manga corta, estampado portada de comic, cuello redondo.",
+    precio: 300,
+    marca: 1,
+    linea: 1,
+  },
+  {
+    id: 2,
+    nombre: "Sudadera Invencible",
+    activo: false,
+    descripcion:
+      "Sudadera invencible con texto en inglés 'Marvel Father's Day Retro Iron Man This Dad Is Invincible'",
+    precio: 400,
+    marca: 1,
+    linea: 2,
+  },
+  {
+    id: 3,
+    nombre: "Tenis total 90",
+    activo: true,
+    descripcion:
+      "OG 2004 Nike Total 90 III TF Pro Botas IC Interior Plata ROJO Elite Air Zoom 10 EE. UU.",
+    precio: 550,
+    marca: 1,
+    linea: 4,
+  },
+];
+
+export default products;
