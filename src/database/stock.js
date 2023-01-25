eb628ad27aaefb2a7c3f38d37583aea2e4a58c19
LuiiVasquez <luivasque95@gmail.com>
2023-01-24 22:19:24 -0600

    creating stock database

---
+++ src/database/stock.js
@@ -0,0 +1,22 @@
+let stock = [
+  {
+    id: 1,
+    activo: true,
+    stock: 200,
+    producto: 1,
+  },
+  {
+    id: 2,
+    activo: true,
+    stock: 100,
+    producto: 2,
+  },
+  {
+    id: 3,
+    activo: true,
+    stock: 300,
+    producto: 3,
+  },
+];
+
+export default stock;
