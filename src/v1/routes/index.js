e0bc1a3d6788de37f456dd84b9f2538b53c2d74d
LuiiVasquez <luivasque95@gmail.com>
2023-01-24 22:26:15 -0600

    adding stock route

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -8,6 +8,7 @@ router.get("/", ({ headers: { host } }, res) => {
     lineas: `http://${host}/lineas`,
     marcas: `http://${host}/marcas`,
     productos: `http://${host}/productos`,
+    stock: `http://${host}/stock`,
   };
 
   res.json({ message: "API menu", menu });
