a47419ce681978e207bbb1a361cd9c5c52c05915
LuiiVasquez <luivasque95@gmail.com>
2023-01-25 20:00:21 -0600

    creating ventas database

---
+++ src/database/ventas.js
@@ -0,0 +1,28 @@
+let ventas = [
+  {
+    id: 1,
+    cantidad: 2,
+    subtotal: 300,
+    montoTotal: 600,
+    producto: 1,
+    fecha_registro: "25-1-2023",
+  },
+  {
+    id: 2,
+    cantidad: 5,
+    subtotal: 300,
+    montoTotal: 1500,
+    producto: 1,
+    fecha_registro: "25-1-2023",
+  },
+  {
+    id: 3,
+    cantidad: 3,
+    subtotal: 550,
+    montoTotal: 1650,
+    producto: 3,
+    fecha_registro: "25-1-2023",
+  },
+];
+
+export default ventas;
