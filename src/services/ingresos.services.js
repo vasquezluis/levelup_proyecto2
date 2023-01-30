39141e7adac85a9d561a6c3ff7b9854254e34940
LuiiVasquez <luivasque95@gmail.com>
2023-01-30 00:09:05 -0600

    creating services for ingresos

---
+++ src/services/ingresos.services.js
@@ -0,0 +1,97 @@
+import databaseIngresos from "../database/ingresos.js";
+import databaseStock from "../database/stock.js";
+
+export const getEntriesService = () => {
+  const entries = databaseIngresos;
+
+  return entries;
+};
+
+export const getiEntryService = (id) => {
+  const entry = databaseIngresos.find((item) => item.id === parseInt(id));
+
+  return entry;
+};
+
+export const createEntryService = ({ producto, cantidad }) => {
+  try {
+    /**
+     * ? lookin for product if in stock databse
+     */
+
+    const stockFound = databaseStock.find(
+      (item) => item.producto == parseInt(producto)
+    );
+
+    if (stockFound) {
+      const id = Math.max(...databaseIngresos.map((item) => item.id)) + 1;
+
+      const dateObject = new Date(Date.now());
+      const date = dateObject.getDate();
+      const month = dateObject.getMonth() + 1;
+      const year = dateObject.getFullYear();
+
+      const newEntry = {
+        id,
+        producto: parseInt(producto),
+        cantidad: parseInt(cantidad),
+        fecha_registro: `${date}-${month}-${year}`,
+      };
+
+      /**
+       * * creating a new record on ingresos database
+       * ! increasing stock on stockFound based on product id
+       */
+
+      databaseIngresos.push(newEntry);
+      stockFound.stock = stockFound.stock + parseInt(cantidad);
+
+      return newEntry;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateEntryService = (id, { cantidad }) => {
+  try {
+    const entryFound = databaseIngresos.find(
+      (item) => item.id === parseInt(id)
+    );
+
+    if (entryFound) {
+      const stockFound = databaseStock.find(
+        (item) => item.producto == entryFound.producto
+      );
+
+      /**
+       * ? actualizar stock de producto dependiendo la cantidad
+       * ! restar si la cantidad es menor al registro
+       * * aumentar si la cantidad es mayor al registro
+       */
+
+      const baseQuantity = entryFound.cantidad;
+
+      if (parseInt(cantidad) < baseQuantity) {
+        stockFound.stock =
+          stockFound.stock - (entryFound.cantidad - parseInt(cantidad));
+        entryFound.cantidad = parseInt(cantidad);
+
+        return entryFound;
+      } else if (parseInt(cantidad) > baseQuantity) {
+        stockFound.stock = stockFound.stock + (cantidad - baseQuantity);
+        entryFound.cantidad = parseInt(cantidad);
+
+        return entryFound;
+      } else {
+        return "Actualizacion de cantidad sin efecto";
+      }
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
