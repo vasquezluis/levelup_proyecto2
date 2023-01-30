244a5c5a929f1cd283e5eccd6888f23da6a61b03
LuiiVasquez <luivasque95@gmail.com>
2023-01-30 00:09:56 -0600

    creating database for ingresos

---
+++ src/database/ingresos.js
@@ -0,0 +1,22 @@
+let entries = [
+  {
+    id: 1,
+    producto: 1,
+    cantidad: 50,
+    fecha_registro: "29-1-2023",
+  },
+  {
+    id: 2,
+    producto: 2,
+    cantidad: 100,
+    fecha_registro: "29-1-2023",
+  },
+  {
+    id: 3,
+    producto: 2,
+    cantidad: 150,
+    fecha_registro: "29-1-2023",
+  },
+];
+
+export default entries;
